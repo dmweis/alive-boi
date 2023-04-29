511493659405387157add3d68dfd66ef6def35cc
David Weis <dweis7@gmail.com>
2023-04-29 17:15:24 +0100

    Update deps

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use clap::{App, Arg};
-use rumqtt::{LastWill, MqttClient, MqttOptions, QoS, ReconnectOptions, Notification};
-use serde::Serialize;
+use anyhow::Context;
 use chrono::prelude::*;
-use log::*;
-use simplelog::*;
+use clap::Parser;
+use rumqtt::{LastWill, MqttClient, MqttOptions, Notification, QoS, ReconnectOptions};
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+use tracing::*;
 
 #[derive(Serialize)]
 enum Status {
@@ -19,70 +20,49 @@ struct AliveReport {
     start_time: DateTime<Local>,
 }
 
-fn main() {
-    if TermLogger::init(LevelFilter::Info, Config::default(), TerminalMode::Mixed).is_err() {
-        eprintln!("Failed to create term logger");
-        if SimpleLogger::init(LevelFilter::Info, Config::default()).is_err() {
-            eprintln!("Failed to create simple logger");
-        }
-    }
+#[derive(Parser, Debug)]
+#[command(
+    version,
+    author = "David M. Weis <dweis7@gmail.com>",
+    about = "Alive Boi"
+)]
+struct Cli {
+    #[arg(long)]
+    mqtt_host: String,
+    #[arg(long)]
+    device_name: String,
+    #[arg(long)]
+    topic: String,
+    #[arg(long)]
+    config: Option<PathBuf>,
+}
 
-    let matches = App::new("Alive boi")
-        .version("1.0")
-        .author("David Weis <dweis7@gmail.com>")
-        .about("Sends MQTT keep alive messages to host")
-        .arg(
-            Arg::with_name("mqtt_host")
-                .long("mqtt_host")
-                .help("points to hostname of MQTT host")
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("device_name")
-                .long("device_name")
-                .help("Name under which the device should appear")
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("topic")
-                .long("topic")
-                .help("Topic for alive and last will messages")
-                .takes_value(true)
-                .required(true),
-        )
-        .get_matches();
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    setup_tracing();
 
-    let mqtt_host = matches
-        .value_of("mqtt_host")
-        .expect("You must provide MQTT host");
-    let device_name = matches
-        .value_of("device_name")
-        .expect("You must provide device_name");
-    let topic = matches
-        .value_of("topic")
-        .expect("You must provide a topic name");
+    let args = Cli::parse();
 
+    let _config = get_configuration(args.config)?;
 
     let start_time: DateTime<Local> = Local::now();
-    
+
     let last_will_message = AliveReport {
-        unit_name: device_name.to_owned(),
+        unit_name: args.device_name.clone(),
         status: Status::Dead,
-        start_time: start_time,
+        start_time,
     };
 
     let last_will = LastWill {
-        topic: topic.to_owned(),
+        topic: args.topic.clone(),
         message: serde_json::to_string(&last_will_message)
-            .expect("Failed to serialize last will message"),
+            .context("Failed to serialize last will message")?,
         qos: QoS::AtLeastOnce,
         retain: true,
     };
 
-    let node_name = format!("alive_boi_{}", &device_name);
-    let mqtt_options = MqttOptions::new(node_name, mqtt_host, 1883)
+    let node_name = format!("alive_boi_{}", &args.device_name);
+    let mqtt_options = MqttOptions::new(node_name, args.mqtt_host.clone(), 1883)
         .set_last_will(last_will)
         .set_keep_alive(5)
         .set_reconnect_opts(ReconnectOptions::Always(5));
@@ -90,17 +70,18 @@ fn main() {
         MqttClient::start(mqtt_options).expect("Failed to connect to MQTT host");
 
     let alive_message = AliveReport {
-        unit_name: device_name.to_owned(),
+        unit_name: args.device_name.clone(),
         status: Status::Alive,
-        start_time: start_time,
+        start_time,
     };
 
     mqtt_client
         .publish(
-            topic,
+            args.topic.clone(),
             QoS::AtLeastOnce,
             true,
-            serde_json::to_string(&alive_message).expect("Failed to serialize last will message"),
+            serde_json::to_string(&alive_message)
+                .context("Failed to serialize last will message")?,
         )
         .expect("Failed to send alive message");
 
@@ -108,26 +89,74 @@ fn main() {
         match notification {
             Notification::Disconnection => {
                 warn!("Client lost connection");
-            },
+            }
             Notification::Reconnection => {
                 warn!("client reconnected");
                 let alive_message = AliveReport {
-                    unit_name: device_name.to_owned(),
+                    unit_name: args.device_name.clone(),
                     status: Status::Reconnected,
-                    start_time: start_time,
+                    start_time,
                 };
                 mqtt_client
                     .publish(
-                        topic,
+                        args.topic.clone(),
                         QoS::AtLeastOnce,
                         true,
-                        serde_json::to_string(&alive_message).expect("Failed to serialize last will message"),
+                        serde_json::to_string(&alive_message)
+                            .context("Failed to serialize last will message")?,
                     )
                     .expect("Failed to send alive message");
-            },
+            }
             other => {
-                warn!("Unexpected message {:?}", other);
+                warn!("Uncontexted message {:?}", other);
             }
         }
     }
+    Ok(())
+}
+
+pub fn setup_tracing() {
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_thread_names(true)
+        .with_max_level(tracing::Level::INFO)
+        .init();
+}
+
+pub fn get_configuration(config: Option<PathBuf>) -> anyhow::Result<MqttConfig> {
+    let mut config_builder = config::Config::builder();
+
+    if let Some(config) = config {
+        info!("Using configuration from {:?}", config);
+        config_builder = config_builder.add_source(config::File::with_name(
+            config
+                .to_str()
+                .ok_or_else(|| anyhow::anyhow!("Failed to convert path"))?,
+        ));
+    } else {
+        info!("Using dev configuration");
+        config_builder = config_builder
+            .add_source(config::File::with_name("configuration/settings"))
+            .add_source(config::File::with_name("configuration/dev_settings"));
+    }
+
+    config_builder = config_builder.add_source(config::Environment::with_prefix("APP"));
+
+    let config = config_builder.build()?;
+
+    Ok(config.try_deserialize::<MqttConfig>()?)
+}
+
+const DEFAULT_MQTT_PORT: u16 = 1883;
+
+const fn default_mqtt_port() -> u16 {
+    DEFAULT_MQTT_PORT
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct MqttConfig {
+    pub broker_host: String,
+    #[serde(default = "default_mqtt_port")]
+    pub broker_port: u16,
+    pub client_id: String,
 }
